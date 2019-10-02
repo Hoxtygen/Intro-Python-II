909bcb9ca14be730e1a2f808430aed285700c5f4
Hoxtygen <hoxtygen@live.com>
2019-10-02 16:07:52 +0100

    Add directional attribute

--- src/room.py
+++ src/room.py
@@ -7,9 +7,25 @@ class Room:
     def __init__(self, name, description):
         self.name = name
         self.description = description
+        self.n_to = None
+        self.s_to = None
+        self.e_to = None
+        self.w_to = None
 
     def getRoomName(self):
         return self.name
 
     def getRoomDescription(self):
-        get self.description
+        return self.description
+
+    def getRoomDirection(self, direction):
+        if direction == 'n':
+            return self.n_to
+        elif direction == 's':
+            return self.s_to
+        elif direction == 'e':
+            return self.e_to
+        elif direction == 'w':
+            return self.w_to
+        else:
+            return None
