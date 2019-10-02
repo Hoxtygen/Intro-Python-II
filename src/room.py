ad5e7e5894958d8f619d2556a964c54d734001f9
Hoxtygen <hoxtygen@live.com>
2019-10-02 14:00:48 +0100

    Add Room class

--- src/room.py
+++ src/room.py
@@ -1,2 +1,15 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+
+class Room:
+    """docstring for Room."""
+
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+
+    def getRoomName(self):
+        return self.name
+
+    def getRoomDescription(self):
+        get self.description
