0129122453869f9decc9a40ea77632052b842fbf
Hoxtygen <hoxtygen@live.com>
2019-10-02 14:16:44 +0100

    Add player class

--- src/player.py
+++ src/player.py
@@ -1,2 +1,15 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+class Player:
+    """docstring for Player."""
+
+    def __init__(self,name='Sub-zero', current_room='outside'):
+        self.current_room = current_room
+
+    def getCurrentRoom(self):
+        return self.current_room
+
+    def getName(self):
+        return self.name
+        
